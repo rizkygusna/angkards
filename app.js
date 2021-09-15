1a106372efa58346f63cd2a73519d19afba8b18b
rizkygusna <rizki.farid1998@gmail.com>
2021-09-15 23:15:28 +0700

    adding style to exercise card

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#optionsForm');
+form.addEventListener('submit', (e) => {
+  // e.preventDefault();
+  const rangeNumber = form.elements.range.value;
+  const cardAmount = form.elements.amount.value;
+  const answerType = form.elements.answer.value;
+});
